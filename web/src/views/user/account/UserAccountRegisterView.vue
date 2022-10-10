80e1390d0828035926f5cf46555b6f263b8b060f
binwatch <h0w1eyu@proton.me>
2022-10-10 16:13:21 +0800

    finish login and register module backend frontend

--- web/src/views/user/account/UserAccountRegisterView.vue
+++ web/src/views/user/account/UserAccountRegisterView.vue
@@ -2,16 +2,20 @@
     <ContentField>
         <div class="row justify-content-md-center">
             <div class="col-3">
-                <form>
+                <form @submit.prevent="register">
                     <div class="mb-3">
                         <label for="username" class="form-label">username</label>
-                        <input type="text" class="form-control" id="username" placeholder="please enter username">
+                        <input v-model="username" type="text" class="form-control" id="username" placeholder="please enter username">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">password</label>
-                        <input type="password" class="form-control" id="password" placeholder="please enter password">
+                        <input v-model="password" type="password" class="form-control" id="password" placeholder="please enter password">
                     </div>
-                    <div class="error-message">wrong password</div>
+                    <div class="mb-3">
+                        <label for="confirmedPassword" class="form-label">confirm password</label>
+                        <input v-model="confirmedPassword" type="password" class="form-control" id="confirmedPassword" placeholder="please enter password again">
+                    </div>
+                    <div class="error-message">{{ error_message }}</div>
                     <button type="submit" class="btn btn-primary">submit</button>
                 </form>
             </div>
@@ -21,9 +25,46 @@
 
 <script>
 import ContentField from "../../../components/ContentField.vue";
+import { ref } from 'vue'
+import router from "../../../router/index"
+import $ from 'jquery';
+
 export default {
     components: {
-    ContentField
+        ContentField
+    },
+    setup() {
+        let username = ref('');
+        let password = ref('');
+        let confirmedPassword = ref('');
+        let error_message = ref('');
+
+        const register = () => {
+            $.ajax({
+                url: "http://127.0.0.1:3000/user/account/register/",
+                type: "post",
+                data: {
+                    username: username.value,
+                    password: password.value,
+                    confirmedPassword: confirmedPassword.value,
+                },
+                success(resp) {
+                    if (resp.error_message === "success") {
+                        router.push({name: "user_account_login"});
+                    } else {
+                        error_message.value = resp.error_message;
+                    }
+                },
+            });
+        }
+
+        return {
+            username,
+            password,
+            confirmedPassword,
+            error_message,
+            register,
+        }
     }
 }
 </script>
